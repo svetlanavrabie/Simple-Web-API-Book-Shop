86536855d04758a1147d4290ce39bf470996f6e8
svetlanavrabie <laurasdamir@gmail.com>
2020-01-29 21:21:59 -0500

    Create the get requests for Categories

---
+++ DemoBookAPI/Services/ICategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoBookAPI.Services
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+
+        Category GetCategory(int categoryId);
+
+        ICollection<Category> GetAllCategoriesForABook(int bookId);
+
+        ICollection<Book> GetBookForCategory(int categoryId);
+
+        bool CategoryExists(int categoryId);
+
+
+    }
+}
